b56af531695b39ff0f2fc975fedb94ff8b6784eb
Michael Lesniak <mail@mlesniak.com>
2016-07-24 10:04:43 +0200

    Parse artist data

--- src/main/scala/com/mlesniak/spark/Main.scala
+++ src/main/scala/com/mlesniak/spark/Main.scala
@@ -8,17 +8,15 @@ import org.apache.spark.{SparkConf, SparkContext}
   * @author Michael Lesniak (mail@mlesniak.com)
   */
 object Main extends App {
+    // We use 64 partitions to have sufficient parallelization on local systems.
+    val minPartitions: Int = 64
+
     val conf = new SparkConf()
         .setAppName("Music playground")
         .setMaster("local[*]")
     val sc: SparkContext = new SparkContext(conf)
 
     def computeStatistics() = {
-        // We use 64 partitions to have sufficient parallelization on local systems.
-        val userDir = System.getProperty("user.dir")
-        var rawUserArtist = sc.textFile(s"file://${userDir}/data/user_artist_data.txt", 64)
-        rawUserArtist.persist()
-
         // Analyze range of user and artist ids.
         val userStats = rawUserArtist.map(_.split(' ')(0).toDouble).stats()
         val artistStats = rawUserArtist.map(_.split(' ')(1).toDouble).stats()
@@ -28,5 +26,25 @@ object Main extends App {
         println(artistStats)
     }
 
-    computeStatistics()
+
+    val userDir = System.getProperty("user.dir")
+    val rawUserArtist = sc.textFile(s"file://$userDir/data/user_artist_data.txt", minPartitions)
+    rawUserArtist.persist()
+
+    val rawArtist = sc.textFile(s"file://$userDir/data/artist_data.txt", minPartitions)
+    var artistByID = rawArtist.flatMap { line =>
+        val (id, name) = line.span(_ != '\t')
+        if (name.isEmpty) {
+            None
+        } else {
+            try {
+                Some((id.toInt, name.trim))
+            } catch {
+                case e: NumberFormatException => None
+            }
+        }
+    }
+    artistByID.persist()
+    println(artistByID.count())
+    artistByID.take(10).foreach(println)
 }
