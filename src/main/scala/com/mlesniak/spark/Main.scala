cf92a47d5dc43bc7bf6e25a6f79fdad4ea4c4e0c
Michael Lesniak <mail@mlesniak.com>
2016-08-03 20:24:56 +0200

    Load saved model for further experiments

--- src/main/scala/com/mlesniak/spark/Main.scala
+++ src/main/scala/com/mlesniak/spark/Main.scala
@@ -1,6 +1,6 @@
 package com.mlesniak.spark
 
-import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
 import org.apache.spark.{SparkConf, SparkContext}
 
 
@@ -18,74 +18,82 @@ object Main extends App {
         .setMaster("local[*]")
     val sc: SparkContext = new SparkContext(conf)
 
-    def computeStatistics() = {
-        // Analyze range of user and artist ids.
-        val userStats = rawUserArtist.map(_.split(' ')(0).toDouble).stats()
-        val artistStats = rawUserArtist.map(_.split(' ')(1).toDouble).stats()
+    //    def computeStatistics() = {
+    //        // Analyze range of user and artist ids.
+    //        val userStats = rawUserArtist.map(_.split(' ')(0).toDouble).stats()
+    //        val artistStats = rawUserArtist.map(_.split(' ')(1).toDouble).stats()
+    //
+    //        println("*** Statistics")
+    //        println(userStats)
+    //        println(artistStats)
+    //    }
+    //
+    //
+    //    val userDir = System.getProperty("user.dir")
+    //    val rawUserArtist = sc.textFile(s"file://$userDir/data/user_artist_data.txt", minPartitions)
+    //    rawUserArtist.persist()
+    //    println("rawUserArtists.count:" + rawUserArtist.count())
+    //
+    //    val rawArtist = sc.textFile(s"file://$userDir/data/artist_data.txt", minPartitions)
+    //    var artistByID = rawArtist.flatMap { line =>
+    //        val (id, name) = line.span(_ != '\t')
+    //        if (name.isEmpty) {
+    //            None
+    //        } else {
+    //            try {
+    //                Some((id.toInt, name.trim))
+    //            } catch {
+    //                case e: NumberFormatException => None
+    //            }
+    //        }
+    //    }
+    //    artistByID.persist()
+    ////    println("ArtistsById:" + artistByID.count())
+    ////    artistByID.take(10).foreach(println)
+    //
+    //    val rawArtistAlias = sc.textFile(s"file://$userDir/data/artist_alias.txt", minPartitions)
+    //    var artistAlias = rawArtistAlias.flatMap { line =>
+    //        val tokens = line.split("\t")
+    //        if (tokens(0).isEmpty) {
+    //            None
+    //        } else {
+    //            Some((tokens(0).toInt, tokens(1).toInt))
+    //        }
+    //    }.collectAsMap()
+    //
+    //    val bArtistAlias = sc.broadcast(artistAlias)
+    //
+    ////    println(s"1====${artistByID.lookup(1)}")
+    ////    println(s"artistAlias.count=${artistAlias.count}")
+    ////    artistAlias.persist()
+    ////    System.exit(1)
+    //
+    ////    val a1 = artistByID.lookup(1092764)
+    ////    val a2 = artistByID.lookup(1000311)
+    ////    println(a1)
+    ////    println(a2)
+    //
+    //    val trainData = rawUserArtist.map { line =>
+    //        val Array(userID, artistID, count) = line.split(" ").map(_.toInt)
+    ////        println(s"\nline=$line")
+    ////        println(s"Mapping $artistID ...")
+    //        val id  =bArtistAlias.value.getOrElse(artistID, artistID)
+    //        val r = Rating(userID, id, count)
+    ////        println(s"Computed rating $r")
+    //        r
+    //    }.cache()
+    //
+    //    println(s"Train data size ${trainData.count()}")
+    //
+    //    println("Training model")
+    //    val model = ALS.trainImplicit(trainData, 10, 5, 0.01, 1.0)
 
-        println("*** Statistics")
-        println(userStats)
-        println(artistStats)
-    }
+    //    println("Saving model")
+    //    model.save(sc, "music-model")
 
-
-    val userDir = System.getProperty("user.dir")
-    val rawUserArtist = sc.textFile(s"file://$userDir/data/user_artist_data.txt", minPartitions)
-    rawUserArtist.persist()
-
-    val rawArtist = sc.textFile(s"file://$userDir/data/artist_data.txt", minPartitions)
-    var artistByID = rawArtist.flatMap { line =>
-        val (id, name) = line.span(_ != '\t')
-        if (name.isEmpty) {
-            None
-        } else {
-            try {
-                Some((id.toInt, name.trim))
-            } catch {
-                case e: NumberFormatException => None
-            }
-        }
-    }
-    artistByID.persist()
-    println(artistByID.count())
-    artistByID.take(10).foreach(println)
-
-    val rawArtistAlias = sc.textFile(s"file://$userDir/data/artist_alias.txt", minPartitions)
-    var artistAlias = rawArtistAlias.flatMap { line =>
-        val tokens = line.split("\t")
-        if (tokens(0).isEmpty) {
-            None
-        } else {
-            Some((tokens(0).toInt, tokens(1).toInt))
-        }
-    }
-
-    artistAlias.persist()
-
-    val a1 = artistByID.lookup(1092764)
-    val a2 = artistByID.lookup(1000311)
-    println(a1)
-    println(a2)
-
-    // Optimize broadcasting for multiple nodes.
-    // val bArtistAlias = sc.broadcast(ArtistArray)
-
-    val trainData = rawUserArtist.map { line =>
-        val Array(userID, artistID, count) = line.split(" ").map(_.toInt)
-        val id = artistAlias.lookup(artistID)
-        val finalID: Int = if (id.isEmpty) {
-            artistID
-        } else {
-            id.head
-        }
-        Rating(userID, finalID, count)
-    }.cache()
-
-    val model = ALS.trainImplicit(trainData, 10, 5, 0.01, 1.0)
-
-    println("Saving model")
-    model.save(sc, "music-model")
     println("Loading model")
+    val model: MatrixFactorizationModel = MatrixFactorizationModel.load(sc, "music-model")
 
-    val loadedModel: MatrixFactorizationModel = MatrixFactorizationModel.load(sc, "music-model")
+    val rdd = model.userFeatures.mapValues(_.mkString(", ")).first()
+    println(rdd)
 }
